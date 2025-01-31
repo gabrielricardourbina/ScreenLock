5eab0c7ec56edc8dc1671cdc3fe9be22c49beab1
Gabriel Ricardo Urbina Royuela <gabrielricardourbina@gmail.com>
2025-01-31 12:50:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,61 +1,45 @@
-const logToPage = (text) => {
-  const pre = document.createElement("pre");
-  pre.innerText = text;
-  document.body.append(pre);
-};
+let abortController = new AbortController();
 
-const takeLock = async () => {
-  try {
+const render = () => {
+  abortController.abort();
+  abortController = new AbortController();
+
+  const button = document.createElement("button");
+  button.classList.add("run");
+  button.classList.add("indicator");
+  button.innerText = "Block ScreenLock";
+
+  button.addEventListener("click", async () => {
+    const context = new AudioContext();
+    const oscillator = context.createOscillator();
+    oscillator.type = "sine";
+    oscillator.frequency.value = 1;
+    const volume = context.createGain();
+    volume.connect(context.destination);
+    volume.gain.value = 0.01;
+    oscillator.connect(volume);
+    oscillator.start();
+
     const wakeLock = await navigator.wakeLock.request("screen");
-    wakeLock.addEventListener("release", () => {
-      logToPage("Error: Wake Lock was released");
+    wakeLock.addEventListener("release", render);
+    button.remove();
+
+    const unlockButton = document.createElement("button");
+    unlockButton.classList.add("run");
+    unlockButton.classList.add("indicator");
+    unlockButton.innerText = "Unblock ScreenLock";
+
+    unlockButton.addEventListener("click", render)
+    document.body.append(unlockButton);
+
+    abortController.signal.addEventListener("abort", () => {
+      unlockButton.remove();
+      context.close();
+      wakeLock.removeEventListener("release", render);
     });
-  } catch (err) {
-    logToPage("Error: Failed to take Wake Lock");
-  }
+  });
+
+  document.body.append(button);
+  abortController.signal.addEventListener("abort", () => button.remove());
 };
-
-const isPopup = !window.menubar.visible;
-if (isPopup) {
-  const button = document.createElement("button");
-  button.classList.add("run");
-  button.classList.add("indicator");
-  button.innerText = "Prevent Screen Lock";
-  button.addEventListener("click", async () => {
-    try {
-      const context = new AudioContext();
-      const oscillator = context.createOscillator();
-      oscillator.type = "sine";
-      oscillator.frequency.value = 1;
-      const volume = context.createGain();
-      volume.connect(context.destination);
-      volume.gain.value = 0.1;
-      oscillator.connect(volume);
-      oscillator.start();
-    } catch (error) {
-      console.error(error);
-    }
-    takeLock();
-    button.remove();
-    const message = document.createElement("div");
-    message.classList.add("indicator");
-    message.innerText = "Locking screen";
-    document.body.append(message);
-  });
-
-  document.body.append(button);
-} else {
-  const button = document.createElement("button");
-  button.classList.add("run");
-  button.classList.add("indicator");
-  button.innerText = "Open In popup";
-  button.addEventListener("click", async () => {
-    window.open(window.location, "_blank", "width=800,height=600");
-    button.remove();
-    const message = document.createElement("div");
-    message.classList.add("indicator");
-    message.innerText = "You can close this tab";
-    document.body.append(message);
-  });
-  document.body.append(button);
-}
+render();
